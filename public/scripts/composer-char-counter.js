d8e28f87041a184b8201f4e8615e1b13a2eeb853
brettg17 <brettgelinas7@gmail.com>
2024-03-19 10:17:16 -0600

    finally completelted user-tweet secion.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
   $("#tweet-text").on("input", function(event) {
       const currentLength = $(this).val().length;
       const remaining = 140 - currentLength;
-      $(".counter").text(remaining);
+      const counter = $(this).closest('form').find('.counter');
+      counter.text(remaining);
 
       if (remaining < 0) {
-          $(".counter").addClass("negative");
+          counter.addClass("negative");
       } else {
-          $(".counter").removeClass("negative");
+          counter.removeClass("negative");
       }
   });
 });
